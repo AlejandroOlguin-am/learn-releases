e64a244147351681cefe08f58a7d835a0f06f6e3
AlejandroOlguin-am <alejandro_olguin17@hotmail.com>
2025-12-30 19:24:27 -0400

    feat: Generacion de carpetas con setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="learnReleases",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Alejandro Olguin",                         # Tu nombre
+    author_email="alejandro@yuravision.com",                 # Tu correo electrónico
+    url="https://github.com/AlejandroOlguin-am/learn-releases",     # URL del proyecto
+)
